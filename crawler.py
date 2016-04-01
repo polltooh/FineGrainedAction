4f69bda4858d2b476ee4a57fda5e088a3cb21fd4
polltooh@gmail.com <Geoff@Guanhangs-MacBook-Pro.local>
2016-03-31 23:14:19 -0400

    crawler

---
+++ crawler.py
@@ -0,0 +1,65 @@
+# import requests
+import urllib2
+from bs4 import BeautifulSoup
+import os
+# from urllib2 import Request
+import json
+from bing_search_api import BingSearchAPI
+import string
+
+def read_bing_key():
+    with open('bing_key.txt', 'r') as f:
+        bing_key = f.read()
+    bing_key = bing_key.replace('\n','')
+    return bing_key
+
+def crawl_from_bing():
+    my_key = read_bing_key()
+    query_string = "nba jumpshot"
+    bing = BingSearchAPI(my_key)
+    for i in range(20):
+        params = {
+              '$format': 'json',
+              '$top': 50,
+              '$skip': i * 50}
+        result_list = bing.search('image',query_string,params).json()
+        print(len(result_list['d']['results'][0]['Image']))
+        for result in result_list['d']['results'][0]['Image']:
+            image_url = (result['MediaUrl'])
+            title_name = result['Title'].encode('gbk', 'ignore').decode(encoding="utf-8", errors="ignore")
+            title_name = title_name.replace('... ','')
+            download_single_image(image_url, query_string, title_name)
+
+def download_single_image(url, query_string, title_name):
+    format = url.split('.')[-1]
+    format_list = format.split("?")
+    if (len(format_list) > 1):
+        format = format_list[0]
+
+    if (format == 'gif'):
+        print('gif')
+        return
+
+    dir_name = "image/" + query_string.replace(' ','_')
+    if not (os.path.isdir(dir_name)):
+        os.mkdir(dir_name)
+
+    valid_chars = "-_() %s%s" % (string.ascii_letters, string.digits)
+    filename = ''.join(c for c in title_name if c in valid_chars)
+    req = urllib2.Request(url)
+    req.add_header('Accept', 
+        'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8')
+    req.add_header('user-agent', 
+        'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_7_4) AppleWebKit/537.13 (KHTML, like Gecko) Chrome/24.0.1290.1 Safari/537.13')
+    try:
+        webpage = urllib2.urlopen(req).read()
+        full_file_name = dir_name + '/' + filename + '.' + format
+        f = open(full_file_name, 'wb')
+        f.write(webpage)
+        f.close()
+    except:
+        print(url)
+
+if __name__ == "__main__":
+    # read_bing_key()
+    crawl_from_bing()

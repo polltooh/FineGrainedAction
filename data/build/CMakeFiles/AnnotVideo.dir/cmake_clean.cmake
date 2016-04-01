489c6793dbf20ff2a8c188382576d89518a499de
Geoff <polltooh@gmail.com>
2016-04-01 18:27:05 -0400

    change dir

---
+++ data/build/CMakeFiles/AnnotVideo.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+FILE(REMOVE_RECURSE
+  "CMakeFiles/AnnotVideo.dir/annotate_video.cpp.o"
+  "CMakeFiles/AnnotVideo.dir/utility_function.cc.o"
+  "AnnotVideo.pdb"
+  "AnnotVideo"
+)
+
+# Per-language clean rules from dependency scanning.
+FOREACH(lang CXX)
+  INCLUDE(CMakeFiles/AnnotVideo.dir/cmake_clean_${lang}.cmake OPTIONAL)
+ENDFOREACH(lang)
---
+++ data/build/CMakeFiles/CleanImage.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+FILE(REMOVE_RECURSE
+  "CMakeFiles/CleanImage.dir/clean_image.cpp.o"
+  "CMakeFiles/CleanImage.dir/utility_function.cc.o"
+  "CleanImage.pdb"
+  "CleanImage"
+)
+
+# Per-language clean rules from dependency scanning.
+FOREACH(lang CXX)
+  INCLUDE(CMakeFiles/CleanImage.dir/cmake_clean_${lang}.cmake OPTIONAL)
+ENDFOREACH(lang)

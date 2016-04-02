d6b3177084b7d80e4bf307698524eda1352db216
polltooh@gmail.com <Geoff@Guanhangs-MacBook-Pro.local>
2016-04-01 21:28:08 -0400

    delete build

--- data/build/CMakeFiles/CleanImage.dir/cmake_clean.cmake
+++ data/build/CMakeFiles/CleanImage.dir/cmake_clean.cmake
@@ -1,4 +1,4 @@
-FILE(REMOVE_RECURSE
+file(REMOVE_RECURSE
   "CMakeFiles/CleanImage.dir/clean_image.cpp.o"
   "CMakeFiles/CleanImage.dir/utility_function.cc.o"
   "CleanImage.pdb"
@@ -6,6 +6,6 @@ FILE(REMOVE_RECURSE
 )
 
 # Per-language clean rules from dependency scanning.
-FOREACH(lang CXX)
-  INCLUDE(CMakeFiles/CleanImage.dir/cmake_clean_${lang}.cmake OPTIONAL)
-ENDFOREACH(lang)
+foreach(lang CXX)
+  include(CMakeFiles/CleanImage.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()

366434e52249967db09d908d63f237cfc198a837
polltooh <polltooh@gmail.com>
2016-04-30 16:17:42 -0400

    end2end train

---
+++ nn/fine_tune_nn_v3.py
@@ -0,0 +1,158 @@
+import tensorflow as tf
+from bvlc_alexnet_fc7 import AlexNet
+import fine_tune_nt
+import nt
+import numpy as np
+import os
+import time
+import cv2
+import image_io
+
+# the dimension of the final layer = feature dim
+LABEL_DIM = 10
+TRAIN_TXT = 'file_list_fine_tune_train_v3.txt'
+# TRAIN_TXT = 'file_list_fine_tune_train.txt'
+# TEST_TXT = 'file_list_test.txt'
+
+TRAIN = True
+SHUFFLE_DATA = True
+BATCH_SIZE = 1
+RADIUS = 10
+FEATURE_ROW = 227
+FEATURE_COL = 227
+
+FLAGS = tf.app.flags.FLAGS
+tf.app.flags.DEFINE_string('train_log_dir','fine_tune_nn_logs',
+        '''directory wherer to write event logs''')
+tf.app.flags.DEFINE_integer('max_training_iter', 10000,
+        '''the max number of training iteration''')
+tf.app.flags.DEFINE_float('init_learning_rate',0.001,
+        '''initial learning rate''')
+tf.app.flags.DEFINE_string('model_dir', 'fine_tune_nn_model_logs','''directory where to save the model''')
+
+def define_graph_config():
+    config_proto =  tf.ConfigProto()
+    config_proto.gpu_options.per_process_gpu_memory_fraction = 0.9
+    return config_proto
+
+def dense_to_one_hot_numpy(labels_dense, num_classes=LABEL_DIM):
+  """Convert class labels from scalars to one-hot vectors."""
+  num_labels = labels_dense.shape[0]
+  index_offset = np.arange(num_labels) * num_classes
+  labels_one_hot = np.zeros((num_labels, num_classes))
+  labels_one_hot.flat[index_offset + labels_dense.ravel()] = 1
+  return labels_one_hot
+
+def dense_to_one_hot(labels_batch, num_classes = LABEL_DIM):
+    sparse_labels = tf.reshape(labels_batch, [-1, 1])
+    derived_size = tf.shape(labels_batch)[0]
+    indices = tf.reshape(tf.range(0, derived_size, 1), [-1, 1])
+    concated = tf.concat(1, [indices, sparse_labels])
+    outshape = tf.pack([derived_size, num_classes])
+    labels = tf.sparse_to_dense(concated, outshape, 1.0, 0.0)
+    return labels
+
+def filequeue_to_batch_data(filename_queue, line_reader, batch_size = BATCH_SIZE):
+   # The text file format should be Query Image, Trieve Image, Query label,
+   # Trieve Label, Triplet loss Label( 0/1 ) 
+
+    key, next_line = line_reader.read(filename_queue)
+    query_image_name, retrieve_image_name, label_1, label_2, label_3 = tf.decode_csv(
+        next_line, [tf.constant([], dtype=tf.string),tf.constant([], dtype=tf.string),
+            tf.constant([], dtype = tf.int32), tf.constant([], dtype = tf.int32), tf.constant([], dtype = tf.int32)], field_delim=" ")
+    
+    # batch_query_image, batch_label = tf.train.batch(
+    #         [query_image_name, label], batch_size=batch_size)
+
+    reverse_channel = True  # for pre-trained purpose
+    query_tensor = image_io.read_image(query_image_name, reverse_channel,   
+            FEATURE_ROW, FEATURE_COL)
+
+    retrieve_tensor = image_io.read_image(retrieve_image_name, reverse_channel,   
+            FEATURE_ROW, FEATURE_COL)
+
+    if SHUFFLE_DATA:
+        min_after_dequeue = 100
+        capacity = min_after_dequeue + 3 * batch_size
+        batch_query_tensor, batch_retrieve_tensor, batch_label_1, batch_label_2, batch_label_3  = tf.train.shuffle_batch(
+                [query_tensor, retrieve_tensor, label_1, label_2, label_3], batch_size = batch_size, capacity=capacity,
+                min_after_dequeue=min_after_dequeue)
+    else:
+        batch_query_tensor,batch_retrieve_tensor, batch_label_1, batch_label_2, batch_label_3 = tf.train.batch(
+                [query_tensor, retrieve_tensor, label_1, label_2, label_3], batch_size=batch_size)
+    
+    batch_tensor = tf.concat(0, [batch_query_tensor, batch_retrieve_tensor]) 
+    batch_label = tf.concat(0, [batch_label_1, batch_label_2])
+    return batch_tensor, batch_label, batch_label_3
+    
+
+def train():
+    train_filenamequeue=tf.train.string_input_producer([TRAIN_TXT], shuffle=SHUFFLE_DATA)
+
+    line_reader = tf.TextLineReader()
+    train_batch_image, train_batch_label, train_batch_label_3 = filequeue_to_batch_data(train_filenamequeue, line_reader)
+
+    train_batch_label_one_hot = dense_to_one_hot(train_batch_label, LABEL_DIM)
+
+    global_step = tf.Variable(0, name = 'global_step', trainable = False)
+    image_data_ph = tf.placeholder(tf.float32, shape = (2 * BATCH_SIZE, FEATURE_ROW, FEATURE_COL, 3))
+    label_ph = tf.placeholder(tf.float32, shape = (2 * BATCH_SIZE, LABEL_DIM))
+    label_ph_3 = tf.placeholder(tf.int32, shape = (BATCH_SIZE))
+
+    net = AlexNet({'data':image_data_ph})
+
+    infer_1 = fine_tune_nt.inference(net.get_output(), LABEL_DIM)
+    loss_1 = fine_tune_nt.loss(infer_1, label_ph)
+
+    loss_2 = nt.triplet_loss(net.get_output(), label_ph_3, BATCH_SIZE, RADIUS)
+    loss = loss_1 + loss_2
+
+    tf.scalar_summary('loss', loss)
+
+    for var in tf.trainable_variables():
+        tf.histogram_summary(var.op.name, var)
+    merged_sum = tf.merge_all_summaries()
+
+    lr = FLAGS.init_learning_rate
+    train_op = fine_tune_nt.training(loss, lr, global_step)
+
+    saver = tf.train.Saver()
+
+    config_proto = define_graph_config()
+    sess = tf.Session(config = config_proto)
+
+    if TRAIN:
+        writer_sum = tf.train.SummaryWriter(FLAGS.train_log_dir,graph_def = sess.graph_def)
+
+    init_op = tf.initialize_all_variables()
+    sess.run(init_op)
+    coord = tf.train.Coordinator()
+    threads = tf.train.start_queue_runners(coord = coord, sess = sess)
+
+    if TRAIN:
+        for i in xrange(FLAGS.max_training_iter):
+            batch_image_v, batch_label_v, batch_label_3_v = sess.run([    
+                train_batch_image, train_batch_label_one_hot, train_batch_label_3])
+
+            feed_data = {image_data_ph: batch_image_v, label_ph: batch_label_v, label_ph_3: batch_label_3_v}
+            loss_v,_ = sess.run([loss, train_op], feed_dict = feed_data) 
+            if i % 100 == 0:
+                print("i:%d, loss:%f"%(i,loss_v))
+            print(batch_label_v)
+            print(batch_label_3_v)
+            exit(1)
+            if i != 0 and i % 500 == 0:
+                curr_time = time.strftime("%Y%m%d_%H%M")
+                model_name = FLAGS.model_dir + '/' + curr_time + '_iter_' + str(i) + '_model.ckpt'
+                saver.save(sess,FLAGS.model_dir + '/' + curr_time + 'model.ckpt')
+
+
+def main(argv = None):
+    if not os.path.exists(FLAGS.model_dir):
+        os.makedirs(FLAGS.model_dir)
+    if not os.path.exists(FLAGS.train_log_dir):
+        os.makedirs(FLAGS.train_log_dir)
+    train()
+
+if __name__ == '__main__':
+    tf.app.run()
---
+++ nn/gen_train_file_v3.py
@@ -0,0 +1,146 @@
+import os
+import cv2
+import random
+import numpy as np
+
+# image_dir = "/home/mscvadmin/action/FineGrainedAction/data/image/"
+# frame_dir = "/home/mscvadmin/action/FineGrainedAction/data/video/"
+
+label_dict = {
+    'nba_neg':'0',
+    'nba_dunk':'1',
+    'nba_jumpshot':'2',
+    'nba_layup':'3',
+    'tennis_neg':'4',
+    'tennis_forehand':'5',
+    'tennis_backhand':'6',
+    'tennis_serve':'7',
+    'baseball_neg':'8',
+    'baseball_hit':'9',
+    'baseball_pitch':'10',
+    'baseball_stolen_base':'11'
+}
+print(label_dict)
+
+
+image_dir = "../data/image/"
+frame_dir = "../data/video/"
+
+def delete_last_empty_line(s):
+    while(len(s) >= 1 and s[-1] == '\n'):
+        s = s[:-1]
+    return s
+
+def get_image(label_name):
+    list_name = os.listdir(image_dir + label_name)
+    list_name = [f for f in list_name if f[0:2] != '._' and f.endswith('.jpg')]
+    list_name = [image_dir + label_name + "/" + f for f in list_name]
+    return list_name
+
+def get_frame(label_name):
+    list_name = os.listdir(frame_dir + label_name)
+    pos_list = list()
+    neg_list = list()
+    for name in list_name:
+       curr_path = frame_dir + label_name + "/" + name + "/"
+       if (os.path.isdir(curr_path)):
+           file_list_name = curr_path + "/file_list.txt"
+           label_list_name = curr_path + "/label.txt"            
+           with open (file_list_name, "r") as f:
+               file_data = (f.read())
+               file_data = delete_last_empty_line(file_data)
+               data_list = file_data.split("\n")
+               # print(data_list)
+           with open (label_list_name, "r") as f:
+               label_data = f.read()
+               label_data = delete_last_empty_line(label_data)
+               label_list = label_data.split("\n")
+           for index in range(len(label_list)):
+               if (label_list[index] == "true"):
+                   pos_list.append(curr_path + data_list[index])
+               else:
+                   neg_list.append(curr_path + data_list[index])
+    return pos_list, neg_list
+
+def get_list(label_name):
+    image = get_image(label_name)
+    frame_pos, frame_neg = get_frame(label_name)
+    return image, frame_pos, frame_neg 
+
+
+def get_neg_name(label_name):
+    neg_tag = label_name.split('_')
+    neg_name = neg_tag[0] + "_neg"
+    return neg_name
+
+def gen_list(file_name_list):
+    # file name 1: the positive image label
+    # file name 2 & 3..: the negative image label
+
+    file_num = len(file_name_list)
+    image_d = dict()
+    frame_pos_d = dict()
+    frame_neg_d = dict()
+    for i in range(file_num):
+        image_d[i], frame_pos_d[i], frame_neg_d[i] = get_list(file_name_list[i])
+
+    label_pos = label_dict[file_name_list[0]]
+    label_neg = label_dict[get_neg_name(file_name_list[0])]
+    # the first neg frame
+    neg_full = frame_neg_d[0]
+    label_neg_list = [label_neg] * len(frame_neg_d[0])
+
+    for i in range(file_num):
+        if i == 0:
+            continue
+        # add another action frame to the negative 
+        neg_full += frame_pos_d[i]
+        label_neg_list += label_dict[file_name_list[i]] * len(frame_pos_d[i])
+
+    time_num = 5
+    pos_index = range(len(frame_pos_d[0]))
+    pos_index = pos_index * time_num
+    pos_index = random.sample(pos_index, time_num * len(image_d[0]))
+    np_pos_index = np.reshape(np.array(pos_index), (len(image_d[0]), time_num))
+    
+    neg_index = range(len(neg_full))
+    neg_index = neg_index * time_num
+    neg_index = random.sample(neg_index, time_num * len(image_d[0]))
+    np_neg_index = np.reshape(np.array(neg_index), (len(image_d[0]), time_num))
+    
+    file_list = list()
+    for i in range(len(image_d[0])):
+        for j in range(time_num):
+            curr_s = image_d[0][i] + " " + frame_pos_d[0][np_pos_index[i][j]] + \
+                " " + label_pos + " " + label_pos + " 1\n"
+            file_list.append(curr_s)
+            curr_s = image_d[0][i] + " " + neg_full[np_neg_index[i][j]] + \
+                " " + label_pos + " " + label_neg_list[np_neg_index[i][j]] + " 0\n"
+            file_list.append(curr_s)
+    if(len(file_list) == 0):
+        print(file_name_list)
+        print(" is wrong")
+    return file_list
+
+
+if __name__ == "__main__":
+    curr_list = list()
+    curr_list += gen_list(['nba_dunk', 'nba_jumpshot', 'nba_layup'])
+    curr_list += gen_list(['nba_jumpshot', 'nba_dunk', 'nba_layup'])
+    curr_list += gen_list(['nba_layup', 'nba_jumpshot', 'nba_dunk'])
+
+    curr_list += gen_list(['tennis_forehand', 'tennis_backhand', 'tennis_serve'])
+    curr_list += gen_list(['tennis_backhand', 'tennis_forehand', 'tennis_serve'])
+    curr_list += gen_list(['tennis_serve', 'tennis_forehand', 'tennis_backhand'])
+
+    curr_list += gen_list(['baseball_hit', 'baseball_pitch', 'baseball_stolen_base'])
+    curr_list += gen_list(['baseball_pitch', 'baseball_hit', 'baseball_stolen_base'])
+    curr_list += gen_list(['baseball_stolen_base', 'baseball_pitch', 'baseball_hit'])
+
+    random.seed(10)
+    random.shuffle(curr_list)
+
+    with open("file_list_train_v3.txt", "w") as f:
+        for c in curr_list:
+            f.write(c)
+

4b6bc1beb82543b374527de2f712df4324646a49
polltooh <polltooh@gmail.com>
2016-05-10 11:05:08 -0400

    load txt

---
+++ nn/load_txt.py
@@ -0,0 +1,13 @@
+# with open("file_list_fine_tune_train_v3.txt") as f:
+#     s = f.read()
+#     s_l = s.split("\n")
+#     for ss in s_l:
+#         ass = ss.split(" ")
+#         if (len(ass) != 5):
+#             print(ass)
+#         # assert(len(ass) == 5)
+
+with open("logs.txt","a+") as f:
+    f.write("aa")
+while(1):
+    pass
